af35e559ee0ab494e465aa35c5986e17ddf20a36
tejj1221 <tejj1221@gmail.com>
2024-07-03 01:05:54 +0530

    updated code

--- app/components/drawingshapes.tsx
+++ app/components/drawingshapes.tsx
@@ -4,14 +4,14 @@ import "../globals.css";
 
 export default function Drawingshapes() {
   return (
-    <div className="drawingshapes  mx-auto mt-10 mb-10">
-      <h2 className="text-3xl font-bold mb-4 ">Customizations on Map</h2>
+    <div className="drawingshapes ">
+      <h2 className=" mb-4 header1">Customizations on Map</h2>
       <p className="mb-6 text-gray-700">
         Draw custom shapes on the map and save them for future reference. You can create, edit, and manage your custom shapes with ease. This is perfect for planning, analysis, and visualization of spatial data.
       </p>
       <div className="features-benifits">
       <div className="features mb-6 border border-gray-200 p-4 rounded-lg shadow-md">
-        <h3 className="text-xl font-semibold mb-2">Features:</h3>
+        <h3 className="header2 mb-2">Features:</h3>
         <ul className="list-disc list-inside space-y-1 text-gray-800">
           <li>Draw polygons, lines, and points</li>
           {/* <li>Save and load your custom shapes</li> */}
@@ -20,7 +20,7 @@ export default function Drawingshapes() {
         </ul>
       </div>
       <div className="benefits mb-6 border border-gray-200 p-4 rounded-lg shadow-md">
-        <h3 className="text-xl font-semibold mb-2">Benefits:</h3>
+        <h3 className="header2 mb-2">Benefits:</h3>
         <ul className="list-disc list-inside space-y-1 text-gray-800">
           <li>Improve spatial planning and analysis</li>
           <li>Visualize data with custom shapes</li>
--- app/components/pointmarker.tsx
+++ app/components/pointmarker.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export default function Pointers() {
   return (
     <div className="pointmarker mx-auto mb-10 pt-5 pb-5">
-      <h2 className="text-2xl font-bold mb-4">Distance Measurement and Marker Management</h2>
-      <p className="text-lg mb-4">
+      <h2 className="text-2xl font-bold mb-4 header1">Distance Measurement and Marker Management</h2>
+      <p className="text-lg mb-4 text-black">
         This feature allows you to measure distances on the map and manage markers. You can add, save, delete, and edit markers on the map.
       </p>
       <Link href="/distance" className="text-blue-500 hover:text-blue-700">
